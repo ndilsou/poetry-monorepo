d032f3b19ebb9cb6c09522b9c462919f2f49b135
ndilsou <nasoungadoy@gmail.com>
2022-08-29 18:59:35 +0100

    add example to test api

---
+++ apps/api/tests/test_main.py
@@ -0,0 +1,10 @@
+from api.main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
